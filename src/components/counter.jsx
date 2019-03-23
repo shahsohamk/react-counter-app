4e66cb29e8b373de704a982f37aa90582eb2538f
shahsohamk <shahsohamk@gmail.com>
2019-03-23 22:24:16 +0530

    Counters component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     //tags: ["tag1", "tag2", "tag3"]
     tags: []
   };
   render() {
     return (
       <div>
+        <h4>
+          Counter#
+          {this.props.id}
+        </h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           className="btn btn-secondary btn-sm"
@@ -23,12 +27,12 @@ class Counter extends Component {
   handleIncrement = product => {
     console.log(product);
     console.log("Increment button clicked");
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   renderTags() {
     if (this.state.tags.length === 0) {
-      return <p>There are no tags</p>;
+      // return <p>There are no tags</p>;
     } else {
       return (
         <ul>
@@ -41,13 +45,13 @@ class Counter extends Component {
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
---
+++ src/components/counters.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 1 },
+      { id: 3, value: 2 },
+      { id: 4, value: 3 },
+      { id: 5, value: 4 }
+    ]
+  };
+  render() {
+    return (
+      <div>
+        {this.state.counters.map(counter => (
+          <Counter key={counter.id} value={counter.value} id={counter.id} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
